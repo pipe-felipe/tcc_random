4a00d658f413a62d0f86211f2047958b69883d34
Felipe Alves <104647149+pipe-felipe@users.noreply.github.com>
2022-07-08 20:37:57 -0300

    CRUD complete (#2)

--- src/main/kotlin/tcc/random/controller/CustomerController.kt
+++ src/main/kotlin/tcc/random/controller/CustomerController.kt
@@ -1,8 +1,11 @@
 package tcc.random.controller
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -17,5 +20,19 @@ class CustomerController(val repository: CustomerRepository) {
     fun createConsumer(@RequestBody customer: Customer) = ResponseEntity.ok(repository.save(customer))
 
     @GetMapping
-    fun read() = ResponseEntity.ok(repository.findAll())
+    fun readConsumers() = ResponseEntity.ok(repository.findAll())
+
+    @PutMapping("{document}")
+    fun updateConsumer(@PathVariable document: String, @RequestBody customer: Customer): ResponseEntity<Customer> {
+        val customerDbOptional = repository.findByDocument(document)
+        val toSave = customerDbOptional
+            .orElseThrow { RuntimeException("Customer document: $document not found") }
+            .copy(name = customer.name)
+        return ResponseEntity.ok(repository.save(toSave))
+    }
+
+    @DeleteMapping("{document}")
+    fun deleteCustomer(@PathVariable document: String) = repository
+        .findByDocument(document)
+        .ifPresent { repository.delete(it) }
 }
