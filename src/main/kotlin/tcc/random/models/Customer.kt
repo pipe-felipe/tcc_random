788d6537ef302633f209df1e9bfd1ee7bc3e2c45
pipe-felipe <alves.felipepipe@gmail.com>
2022-07-12 19:05:36 -0300

    WIP

--- src/main/kotlin/tcc/random/models/Customer.kt
+++ src/main/kotlin/tcc/random/models/Customer.kt
@@ -1,10 +1,8 @@
 package tcc.random.models
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
-import tcc.random.repositories.CreditCard
 
 @Document
 data class Customer(
@@ -13,6 +11,5 @@ data class Customer(
     val name: String,
     @Indexed(unique = true)
     val document: String,
-    @JsonProperty("creditCard")
-    val creditCard: CreditCard?
+    val creditCard: CreditCard? = null
 )
